950cbea47b6501dd68e8fce3859429455c011ff7
Juan Borrajo Rodriguez <juanborrajo10@gmail.com>
2018-04-11 08:40:11 +0200

    Inicio de Proyecto

---
+++ src/boletin23/Boletin23.java
@@ -0,0 +1,15 @@
+
+package boletin23;
+
+/**
+ *
+ * @author Juan Borrajo Rodriguez
+ */
+public class Boletin23 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
