4ca837e5493343148514a4e08b72d9c42b482fd9
Juan Borrajo Rodriguez <juanborrajo10@gmail.com>
2018-04-11 09:37:17 +0200

    Clase main

--- src/boletin23/Boletin23.java
+++ src/boletin23/Boletin23.java
@@ -1,6 +1,11 @@
 
 package boletin23;
 
+import boletin23.ejercicio3.SaudoDespedida;
+import boletin23.ejercicio2.CursoDam;
+import boletin23.ejercicio1.Premer;
+import boletin23.ejercicio4.Ventana1;
+
 /**
  *
  * @author Juan Borrajo Rodriguez
@@ -9,7 +14,10 @@ public class Boletin23 {
 
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        Premer pre=new Premer();
+        CursoDam cur=new CursoDam();
+        SaudoDespedida sa=new SaudoDespedida();
+        Ventana1 mas=new Ventana1();
     }
 
 }
